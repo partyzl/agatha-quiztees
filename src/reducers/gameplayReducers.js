42bc78c343d926f5eeb775f53eff07f8206b9c46
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-20 15:34:20 +0100

    test coverage 100% for reducers

--- src/reducers/gameplayReducers.js
+++ src/reducers/gameplayReducers.js
@@ -10,7 +10,7 @@ const initState =
 const gameplayReducer = (state = initState, action) => {
     switch (action.type) {
         case 'ANSWER_QUESTION':
-            return { ...state, answers: action.payload.answers, score: action.payload.score };
+            return { ...state, answers: action.payload.answers, score: action.payload.score, stats: action.payload.stats };
         case 'NEXT_QUESTION':
             return { ...state, currentIndex: state.currentIndex + 1 };
         default:
