441d9a1642ed2adb32b3e574e1341af496f8e51c
Parth Patel <parth6796@yahoo.com>
2021-09-20 17:08:15 +0100

    added Model tests

--- server/tests/unit/models/scores.spec.js
+++ server/tests/unit/models/scores.spec.js
@@ -1,32 +1,40 @@
-const Score = require('../../../models/Scores');
-const db = require('../../../dbConfig')
-const pg = require('pg');
-jest.mock('pg');
+const Score = require("../../../models/Scores");
+const db = require("../../../dbConfig");
+const pg = require("pg");
+jest.mock("pg");
 
-describe('Scores', () => {
-    
-  beforeEach(() => jest.clearAllMocks())
-    
-    afterAll(() => jest.resetAllMocks())
+describe("Scores", () => {
+  beforeEach(() => jest.clearAllMocks());
 
-    describe('all', () => {
-        test('it resolves with all scores data on successful request to db', async () => {
-            jest.spyOn(db, 'query')
-                .mockResolvedValueOnce({ rows: {...scores}});
-            const all = await Score.all
-            expect(all).toHaveLength(1) 
-        })
+  afterAll(() => jest.resetAllMocks());
+
+  describe("all", () => {
+    test("it resolves with all scores data on successful request to db", async () => {
+      jest
+        .spyOn(db, "query")
+        .mockResolvedValueOnce({ rows: [{}, {}, {}, {}, {}] });
+      const all = await Score.all;
+      expect(all).toHaveLength(5);
     });
 
-    })
-
-    describe('create', () => {
-        test('it resolves with scores data on successful db enquiry', async () => {
-            let scoresData = { username: 'BowlOfSoup', score: 3 }
-            jest.spyOn(db, 'query')
-                .mockResolvedValueOnce({rows: [ { ...scoresData, id: 1 }] });
-            const result = await Scores.create(scoresData);
-            expect(result).toHaveProperty('username')
-        })
+    test("returns error when fails to get scores", async () => {
+      try {
+        jest.spyOn(db, "query").mockRejectedValueOnce(Error());
+        await Score.all;
+      } catch (err) {
+        expect(err).toContain("No scores for you!Error");
+      }
     });
-    
+  });
+
+  describe("create", () => {
+    test("it resolves with scores data on successful db enquiry", async () => {
+      let scoresData = { username: "BowlOfSoup", score: 3 };
+      jest
+        .spyOn(db, "query")
+        .mockResolvedValueOnce({ rows: [{ ...scoresData, id: 1 }] });
+      const result = await Scores.create(scoresData);
+      expect(result).toHaveProperty("username");
+    });
+  });
+});
