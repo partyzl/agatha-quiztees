d228cef2b61f6b7051ac03c280de72bd26cd903a
teganGowing <tgowing97@gmail.com>
2021-09-20 15:46:05 +0100

    score banner component

---
+++ src/components/Scorebanner/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./style.css";
+
+const Scorebanner = () => {
+    return (
+
+        <div class='banner'>
+            <div class='position'>
+                <p>{position}</p> 
+            </div>
+            <div class='name'>
+                <p>{username}</p>
+            </div>
+            <div class='score'>
+                <p>{score}</p>
+            </div>
+        </div>
+
+    );
+}
+
+export default Scorebanner;
