fe07f9ef34a6e654353648a99adc26f88cf7ede3
Simon Allan <84972531+Sallan464@users.noreply.github.com>
2021-09-20 18:07:26 +0100

    added useStates for quiz page rendering

--- src/Pages/Quiz/index.js
+++ src/Pages/Quiz/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useDispatch,useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { useHistory } from 'react-router-dom';
+// will need something here to import questions
+// will need something here to import an action like fetchQuestions from actions
 
 const Quiz = () => {
+    const dispatch = useDispatch();
+
+    const [ actualUser, setActualUser ] = useState(0);
+    const [ actualQuestion, setActualQuestion ] = useState(0);
+    const [ isFirstRun, setIsFirstRun ] = useState(true);
+
+    const [ toggle, setToggle ] = useState(false)
+    const { level } = useParams();
+    const history = useHistory();
+    const quizData = useSelector(state => state.settings);
+    const categoryData = useSelector(state => state.categories);
+    const questions = useSelector(state=> state.questions);
+    const scores = useSelector(state=> state.scores);
+    
+    }
+    
     return (
         <div>
             
         </div>
     );
-}
+
 
 export default Quiz;
