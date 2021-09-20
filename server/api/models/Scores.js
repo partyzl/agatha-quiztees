6ddf3717ce3492740446f57c64dff66f8f1f8a4c
Simon Allan <stallan464@googlemail.com>
2021-09-20 10:07:02 +0100

    flubflubflub

--- server/api/models/Scores.js
+++ server/api/models/Scores.js
@@ -22,19 +22,20 @@ class Score {
         })
     }
 
-    static get byUserName() {
+    static byUserName() {
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init()
                 const scoreData = await db.query(`INSERT SQL HERE;`);
-                // const scoresByUserName = scoreData
+                const scoresByUserName = scoreData
                 resolve(scoresByUserName);
             } catch (err) {
                 reject("Sorry! Couldn't get that Username!")
             }
+        })
     }
 
-    static create (scores){
+    static create (scores) {
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init();
