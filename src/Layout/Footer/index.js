417bf728f3fd4ff4f11fd8491b2eb870c812b073
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-20 14:25:41 +0100

    Navigation

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer id="footer">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-md-6 footer-contact">
-              <h3>Agatha Quiztees</h3>
-            </div>
+  return (
+    <footer id="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-6 footer-contact">
+            <h3>Agatha Quiztees</h3>
           </div>
-  
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li>
-                <i class="bx bx-chevron-right"></i> <a href="/">Home</a>
-              </li>
-              <li>
-                <i class="bx bx-chevron-right"></i> <a href="#">About us</a>
-              </li>
-            </ul>
-          </div>
-  
-          <div class="container d-md-flex py-4">
-            <div class="me-md-auto text-center text-md-start">
-              <div class="copyright">
-                &copy; Copyright{" "}
-                <strong>
-                  <span>Agatha Quiztees</span>
-                </strong>
-                . All Rights Reserved.
-              </div>
+        </div>
+
+        <div className="col-lg-2 col-md-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li>
+              <i className="bx bx-chevron-right"></i> <a href="/">Home</a>
+            </li>
+            <li>
+              <i className="bx bx-chevron-right"></i> <a href="#">About us</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="container d-md-flex py-4">
+          <div className="me-md-auto text-center text-md-start">
+            <div className="copyright">
+              &copy; Copyright{" "}
+              <strong>
+                <span>Agatha Quiztees</span>
+              </strong>
+              . All Rights Reserved.
             </div>
           </div>
         </div>
-      </footer>
-    );
-}
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import Header from "../../Layout/Header";
+import Footer from "../../Layout/Footer";
 const Landing = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
 
 export default Landing;
