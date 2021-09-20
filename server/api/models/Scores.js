05dbd53e43f950be33c3c6e2dc1524f26633016b
Simon Allan <stallan464@googlemail.com>
2021-09-20 10:44:45 +0100

    modified models findByUsername and create new scoreEntry

--- server/api/models/Scores.js
+++ server/api/models/Scores.js
@@ -9,12 +9,15 @@ class Score {
     this.score = data.score
 }
 
+// Getting Top 10 leaderboard
     static get all() {
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init()
-                const scoreData = await db.query(`INSERT SQL HERE;`); //For when Tegan does the db
-                const scores = scoreData.rows.map(s => new Score({ ...s, id: s._id }))
+                const scoreData = await db.query(`SELECT * FROM scores
+                                                  ORDER BY score DESC LIMIT 10;`); 
+                // const scores = scoreData.rows.map(s => new Score({ ...s, id: s._id }))
+                const scores = scoreData.rows.map(s => new Score(s))
                 resolve(scores);
             } catch (err) {
                 reject("No scores for you!")
@@ -22,12 +25,14 @@ class Score {
         })
     }
 
-    static byUserName() {
+// Finding score info by Username
+    static findByUsername(username) {
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init()
-                const scoreData = await db.query(`INSERT SQL HERE;`);
-                const scoresByUserName = scoreData.rows.map(s => new Score({ ...s, id: s._id }))
+                const scoreData = await db.query(`SELECT * FROM scores 
+                                                  WHERE username = $1;`, [ username ]);
+                const scoresByUserName = scoreData.rows.map(s => new Score(s))
                 resolve(scoresByUserName);
             } catch (err) {
                 reject("Sorry! Couldn't get that Username!")
@@ -35,11 +40,13 @@ class Score {
         })
     }
 
-    static create (scores) {
+// creating a new score entry
+    static create (scoreEntry) {
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init();
-                const scoreData = await db.query(`INSERT INTO scores etc etc;`); //For when Tegan does the db
+                const { username, category, score } = scoreEntry
+                const scoreData = await db.query(`INSERT INTO scores (username, category, score) VALUES ($1, $2, $3) RETURNING *;`, [username, category, score ]); 
                 let newScore = new Score(scoreData.rows[0]);
                 resolve (newScore);
                 console.log('Data Saved to Database');
