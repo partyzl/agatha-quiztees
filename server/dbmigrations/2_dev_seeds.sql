2832acb0d46b6f23d5e8447a99416abcb066d38b
Simon Allan <stallan464@googlemail.com>
2021-09-20 10:53:31 +0100

    expand seed

--- server/dbmigrations/2_dev_seeds.sql
+++ server/dbmigrations/2_dev_seeds.sql
@@ -1,6 +1,14 @@
--- INSERT INTO scores (username, category, score) 
--- VALUES
--- ('Ollie', 'Science', 25); 
+INSERT INTO scores (username, category, score) 
+VALUES
+('Ollie', 'Science', 25),
+('Jules', 'music', 56),
+('Ollie', 'literature', 23),
+('Maeve', 'science', 42),
+('Liam', 'science', 41),
+('Rob', 'music', 32),
+('Sara', 'music', 51);
+
+
 
 
 
