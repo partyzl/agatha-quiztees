e65603c139e2f7326e50a3caeca1e2a5e7685cf2
Parth Patel <parth6796@yahoo.com>
2021-09-21 09:53:05 +0100

    moved db file out of server

--- server/dbmigrations/seedDev.js
+++
@@ -1,6 +0,0 @@
-// const db = require('../api/dbconfig/init');
-// const fs = require('fs');
-
-// const seeds = fs.readFileSync(__dirname + '/dev_seeds.sql').toString();
-
-// db.query(seeds, () => console.log('Dev database seeded'));
