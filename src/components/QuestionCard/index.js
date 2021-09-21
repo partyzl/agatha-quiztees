3608d071994fd919ea56e99a84aa98372a095c68
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-21 17:26:08 +0100

    used dangeriouslyInnerHTML for question text

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -15,9 +15,11 @@ const QuestionCard = ({ round }) => {
     const options = randomiser([round.correct_answer, ...round.incorrect_answers])
 
     useEffect(() => {
-        if (selection){ dispatch(answerQuestion(''))}
+        if (selection) { dispatch(answerQuestion('')) }
         const renderOptions = () => {
-            return options.map(choice => <button className={`option ${showAnswer(choice)} ${isSelected(choice)}`} disabled={answered} onClick={clickHandler}>{choice}</button>)
+            return options.map(choice => (
+                <button className={`option ${showAnswer(choice)} ${isSelected(choice)}`} disabled={answered} onClick={clickHandler} dangerouslySetInnerHTML={{__html: choice }} />)
+            )
         }
         setOptionElements(renderOptions)
         return () => setSelection(null)
@@ -35,7 +37,7 @@ const QuestionCard = ({ round }) => {
     }
 
     function isSelected(option) {
-        const classStyling = option == selection? 'selected' : '';
+        const classStyling = option == selection ? 'selected' : '';
         return classStyling
     }
 
@@ -45,9 +47,7 @@ const QuestionCard = ({ round }) => {
 
     return (
         <article>
-            <div className='question'>
-                <p>{round.question}</p>
-            </div>
+            <div className='question' dangerouslySetInnerHTML={{ __html: round.question }} />
             <div className='choices'>
                 {optionElements}
             </div>
@@ -56,71 +56,3 @@ const QuestionCard = ({ round }) => {
 }
 
 export default QuestionCard;
-
-
-
-
-
-
-
-
-//simons code
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import Timebar from '../Timebar';
-// import './style.css'
-
-// function Question({ question, selected }) {
-//     const [options, setOptions] = useState([]);
-//     const [input, setInput] = useState('');
-//     const [isFirstRun, setIsFirstRun] = useState(true);
-
-//     useEffect(() => {
-//         const newOptions = randomizer([question.correct_answer, ...question.incorrect_answers]);
-//         setOptions(newOptions);
-//         showOptions()
-//     }, [])
-
-//     useEffect(() => {
-//         if (isFirstRun) {
-//             setIsFirstRun(false);
-//             return;
-//         }
-//         else { selected(input) }
-//     }, [input])
-
-//     function randomizer(array) {
-//         const randomizedArray = [];
-//         const copyArray = array;
-//         while (copyArray.length > 1) {
-//             randomizedArray.push(copyArray.splice(Math.floor(Math.random() * copyArray.length), 1)[0])
-//         }
-//         randomizedArray.push(copyArray.pop())
-//         return randomizedArray;
-//     }
-
-//     function showOptions() {
-//         return options.map((option, i) => {
-//             return (<label key={i}> {renderHTML(option, 'p')}
-//                 <input type='radio' name='option' value={option} onChange={(e) => setInput(e.target.value)} />
-//             </label>
-//             )
-//         });
-//     }
-
-//     return (
-
-//         <div className='question'>
-//             {renderHTML(question.question, 'h3')}
-//             <div className='options'>
-//                 {options && showOptions()}
-//             </div>
-//             <Timebar duration='10000' endAction={(e) => setInput(e)} />
-//         </div>
-//     );
-
-// }
-
-// export default Question;
