3ffc81852e653f849280663c4d2b39b088ba8648
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-21 21:22:43 +0100

    pushing update to app.js

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { Landing } from "./Pages";
+import { Setup } from "./Pages";
+import { WaitingRoom } from "./Pages";
+import { Quiz } from "./Pages";
+import { Results } from "./Pages";
+import { Leaderboard } from "./Pages";
+import { JoinQuiz } from "./Pages";
+
+function App() {
+  return (
+    <div id="app">
+      <main>
+        <Switch atEnter>
+          <Route exact path="/" component={Landing}/>
+          <Route path="/setup" component={Setup}/>
+          <Route path="/waitingroom" component={WaitingRoom}/>
+          <Route path="/quiz" component={Quiz}/>
+          <Route path="/results" component={Results}/>
+          <Route path="/leaderboard" component={Leaderboard}/>
+          <Route path="/joiningquiz" component={JoinQuiz}/>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
