3d488431c71c745a50541c335663400e0fee85b9
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-21 18:10:20 +0100

    fixed random conflicts

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -4,8 +4,4 @@ export { default as Leaderboard } from "./Leaderboard";
 export { default as Quiz } from "./Quiz";
 export { default as Setup } from "./Setup";
 export { default as WaitingRoom } from "./WaitingRoom";
-<<<<<<< HEAD
 export { default as Results } from "./Results"
-=======
-export { default as Results } from "./Results";
->>>>>>> 07b940aaacec5d4b1a25b20861113b663fcd0626
