8d127c5a0097cca8091718a8b16b9c689bb783c8
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-21 22:01:10 +0100

    general styling

--- src/App.js
+++ src/App.js
@@ -4,37 +4,22 @@ import { Landing } from "./Pages";
 import { Setup } from "./Pages";
 import { WaitingRoom } from "./Pages";
 import { Quiz } from "./Pages";
-// import { Results } from "./Pages";
+import { Results } from "./Pages";
 import { Leaderboard } from "./Pages";
 import { JoinQuiz } from "./Pages";
-
 
 function App() {
   return (
     <div id="app">
       <main>
-        <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/setup">
-            <Setup />
-          </Route>
-          <Route path="/waitingroom">
-            <WaitingRoom />
-          </Route>
-          <Route path="/quiz">
-            <Quiz />
-          </Route>
-          {/* <Route path="/results">
-            <Results />
-          </Route> */}
-          <Route path="/leaderboard">
-            <Leaderboard />
-          </Route>
-          <Route path="/joinquiz">
-            <JoinQuiz />
-          </Route>
+        <Switch atEnter>
+          <Route exact path="/" component={Landing}/>
+          <Route path="/setup" component={Setup}/>
+          <Route path="/waitingroom" component={WaitingRoom}/>
+          <Route path="/quiz" component={Quiz}/>
+          <Route path="/results" component={Results}/>
+          <Route path="/leaderboard" component={Leaderboard}/>
+          <Route path="/joiningquiz" component={JoinQuiz}/>
         </Switch>
       </main>
     </div>
--- src/Pages/Leaderboard/index.js
+++ src/Pages/Leaderboard/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Header } from '../../Layout';
-import { Footer } from '../../Layout';
+import React from "react";
+import { useHistory } from "react-router";
+import { Header } from "../../Layout";
+import { Footer } from "../../Layout";
+import { Button } from "../../components";
+
 
 const Leaderboard = () => {
-    return ( 
-        <>
-        <Header />
-        <Footer />
-        </>
-    );
-}
+  let history = useHistory();
+  const landing = () => {
+    history.push("/");
+  };
+  return (
+    <>
+      <Header />
+      <Button value="Landing" onClick={landing} />
+      <Footer />
+    </>
+  );
+};
 
 export default Leaderboard;
