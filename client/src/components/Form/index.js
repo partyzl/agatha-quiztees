25c34cbf67bdb80c3d8714adfeb20edd9e47172b
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-22 14:28:06 +0100

    form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -21,12 +21,13 @@ const Form = () => {
   const submitHandler = async (event) => {
     const settings = {
       amount: 10,
-      category: 9,
+      category: category,
       difficulty: difficulty,
       type: question,
     };
     event.preventDefault();
     await getQuestions(dispatch, settings);
+    console.log(category);
     history.push("/quiz");
   };
 
@@ -50,10 +51,25 @@ const Form = () => {
               setCategory(e.target.value);
             }}
           >
-            <option value="1">General Knowledge</option>
-            <option value="2">Mythology</option>
-            <option value="3">Art</option>
-            <option value="history">History</option>
+            <option value="9">General Knowledge</option>
+            <option value="10">Books</option>
+            <option value="11">Film</option>
+            <option value="12">Music</option>
+            <option value="13">Musicals & Theatres</option>
+            <option value="14">Television</option>
+            <option value="15">Video Games</option>
+            <option value="16">Board Games</option>
+            <option value="17">Science & Nature</option>
+            <option value="19">Science and Maths</option>
+            <option value="20">Mythology</option>
+            <option value="22">Geography</option>
+            <option value="21">Sports</option>
+            <option value="25">Art</option>
+            <option value="23">History</option>
+            <option value="26">Celebrities</option>
+            <option value="30">Science: Gadgets</option>
+            <option value="31">Anime & Manga</option>
+            <option value="32">Cartoons</option>
           </select>
           <label htmlFor="players">Players</label>
           <select
