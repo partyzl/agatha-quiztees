f99920743a186c4af88aa1a10da53095ec1a42db
Tegan Gowing <tgowing97@gmail.com>
2021-09-22 16:04:40 +0100

    waiting room question particles

--- client/src/Pages/WaitingRoom/index.js
+++ client/src/Pages/WaitingRoom/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import { Header } from '../../Layout';
 import { Footer } from '../../Layout';
 
 const WaitingRoom = () => {
     return (
         <>
+          <div>
+            <Particles
+              params={{
+                particles:{
+                  number: {
+                    value: 30,
+                    density:{
+                      enable:true,
+                      value_area: 900
+                    }
+                  },
+                  "move": {
+                    "speed": 1,
+                    "out_mode": "out"
+                },
+                  shape:{
+                    "type": "image",
+                    "image":
+                        {
+                            "src": "../../../public/questionmarksvg.svg",
+                            "height": 20 
+                        }
+                        }
+                }
+                  }} />
+                  </div>
         <Header />
+        <Particles />
         <Footer />
         </>
     );
