1a6597227883aba19951b0dd8614901e66d9fbd1
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-22 12:29:28 +0100

    form works

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,81 +1,98 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
+import { getQuestions } from "../../actions";
+import "./styles.css";
 
 const Form = () => {
-  let history = useHistory();
+  const history = useHistory();
+  const dispatch = useDispatch();
   const [username, setUsername] = useState("");
-  const [category, setCategory] = useState("");
-  const [players, setPlayers] = useState("");
-  const [difficulty, setDifficulty] = useState("");
-  const [question, setQuestions] = useState("");
+  const [category, setCategory] = useState("generalKnowledge");
+  const [players, setPlayers] = useState("player1");
+  const [difficulty, setDifficulty] = useState("easy");
+  const [question, setQuestions] = useState("boolean");
 
-  const submitHandler = (event) => {
+  useEffect(
+    () => console.log([username, category, players, difficulty, question]),
+    [username, category, players, difficulty, question]
+  );
+
+  const submitHandler = async (event) => {
+    const settings = {
+      amount: 10,
+      category: 9,
+      difficulty: difficulty,
+      type: question,
+    };
     event.preventDefault();
-
+    await getQuestions(dispatch, settings);
     history.push("/quiz");
   };
 
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="uname">Username</label>
-        <input
-          type="text"
-          name="uname"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <label htmlFor="category">Category</label>
-        <select
-          value={category}
-          onChange={(e) => {
-            setCategory(e.target.value);
-          }}
-        >
-          <option value="generalKnowledge">General Knowledge</option>
-          <option value="mythology">Mythology</option>
-          <option value="art">Art</option>
-          <option value="history">History</option>
-        </select>
-        <label htmlFor="players">Players</label>
-        <select
-          value={players}
-          onChange={(e) => {
-            setPlayers(e.target.value);
-          }}
-        >
-          <option value="player1">1 Player</option>
-          <option value="player2">2 Players</option>
-          <option value="player3">3 Players</option>
-          <option value="player4">4 Players</option>
-        </select>
+    <div className="formContainer">
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <label htmlFor="uname">Username</label>
+          <input
+            type="text"
+            name="uname"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <label htmlFor="category">Category</label>
+          <select
+            value={category}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+          >
+            <option value="1">General Knowledge</option>
+            <option value="2">Mythology</option>
+            <option value="3">Art</option>
+            <option value="history">History</option>
+          </select>
+          <label htmlFor="players">Players</label>
+          <select
+            value={players}
+            onChange={(e) => {
+              setPlayers(e.target.value);
+            }}
+          >
+            <option value="player1">1 Player</option>
+            <option value="player2">2 Players</option>
+            <option value="player3">3 Players</option>
+            <option value="player4">4 Players</option>
+          </select>
 
-        <label htmlFor="difficulty">Difficulty</label>
-        <select
-          value={difficulty}
-          onChange={(e) => {
-            setDifficulty(e.target.value);
-          }}
-        >
-          <option value="easy">Easy</option>
-          <option value="medium">Medium</option>
-          <option value="hard">Hard</option>
-        </select>
-        <label htmlFor="question">Question Type</label>
-        <select
-          value={question}
-          onChange={(e) => {
-            setQuestions(e.target.value);
-          }}
-        >
-          <option value="truefalse">True or False</option>
-          <option value="multiple">Multiple Choice</option>
-        </select>
-        <input type="submit" />
-      </form>
-    </>
+          <label htmlFor="difficulty">Difficulty</label>
+          <select
+            value={difficulty}
+            onChange={(e) => {
+              setDifficulty(e.target.value);
+            }}
+          >
+            <option value="easy">easy</option>
+            <option value="medium">medium</option>
+            <option value="hard">hard</option>
+          </select>
+          <label htmlFor="question">Question Type</label>
+          <select
+            value={question}
+            onChange={(e) => {
+              setQuestions(e.target.value);
+            }}
+          >
+            <option value="boolean">True or False</option>
+            <option value="multiple">Multiple Choice</option>
+          </select>
+          <input className="submit" type="submit" />
+        </form>
+      </div>
+    </div>
   );
 };
 
