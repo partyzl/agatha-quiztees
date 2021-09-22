78d525242c7eb63f05544e3da1df8e9f06337a44
Parth Patel <parth6796@yahoo.com>
2021-09-22 09:34:07 +0100

    scoreline function

--- client/src/Pages/Leaderboard/index.js
+++ client/src/Pages/Leaderboard/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Header } from '../../Layout';
-import { Footer } from '../../Layout';
+import React, { useState, useEffect } from "react";
+import { Header } from "../../Layout";
+import { Footer } from "../../Layout";
+import axios from "axios";
+import { Leaderboardlist } from "../../components";
+import "./style.css";
 
 const Leaderboard = () => {
-    return ( 
-        <>
-        <Header />
-        <Footer />
-        </>
-    );
-}
+  const scoreLine = leaderboard.map((item, i) => (
+    <div className="scores-container" key={i}>
+      <Leaderboardlist scores={item} />
+    </div>
+  ));
+  return (
+    <>
+      <Header />
+      <div className="flex-container">
+        <h1> LEADERBOARD </h1>
+
+        {/* <Leaderboardlist /> */}
+        {error ? <p>{error}</p> : scoreLine}
+        {/* <Footer /> */}
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Leaderboard;
+
+const Leaderboard = () => {
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(async () => {
+    const { data } = await axios.get("http://localhost:3000/leaderboard/");
+    setLeaderboard(data.scores);
+  }, []);
+};
