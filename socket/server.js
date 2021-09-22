1ac72ce5dd49179b347280f72ecb766bfa839b1a
Parth Patel <parth6796@yahoo.com>
2021-09-22 11:32:25 +0100

    added cors to socket

--- socket/server.js
+++ socket/server.js
@@ -1,4 +1,8 @@
-const io = require("socket.io")(3002);
+const io = require("socket.io")(3002, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log(socket.id);
