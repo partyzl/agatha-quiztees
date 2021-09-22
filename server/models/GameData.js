779fdf57e554cbd699a7c9d0a632471fd9fc6a95
Parth Patel <parth6796@yahoo.com>
2021-09-22 15:24:35 +0100

    gameData model added

---
+++ server/models/GameData.js
@@ -0,0 +1,10 @@
+class GameData {
+  constructor(settings, questions, playersArr) {
+    this.settings = settings;
+    this.currentIdx = 0;
+    this.questions = questions;
+    this.playersArr = genPlayers(playersArr);
+  }
+}
+
+module.exports = GameData;
