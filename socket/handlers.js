7b52fcca268f89c0902a25af341a03996057ccde
Parth Patel <parth6796@yahoo.com>
2021-09-22 15:53:08 +0100

    handler functions set up

---
+++ socket/handlers.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const gameStart = () => {
+  console.log("hello world");
+};
+
+const hasPlayerAnswered = () => {};
+
+module.exports = {
+  gameStart,
+  hasPlayerAnswered,
+};
