b005c8e509956aef9b511c298d8892015a0d5470
Parth Patel <parth6796@yahoo.com>
2021-09-22 15:53:45 +0100

    pencilled in functions

--- socket/server.js
+++ socket/server.js
@@ -1,9 +1,23 @@
-const io = require("socket.io")(3002, {
+const GameData = require("../server/models/GameData");
+const playerData = require("../server/models/playerData");
+const gameStart, hasPlayerAnswered = require("./handlers")
+
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
   cors: {
     origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  socket.on("game:start", gameStart);
+  socket.on("player:answer", hasPlayerAnswered);
 });
+
+httpServer.listen(3002);
