1b6939fcd470e3f3aaf11aca94d132a7c18a15e5
Simon Allan <stallan464@googlemail.com>
2021-09-23 10:26:11 +0100

    leaderboard testing

--- client/src/components/Button/test.js
+++ client/src/components/Button/test.js
@@ -1,19 +1,14 @@
-import { render } from 'react-dom';
-import {screen, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import axios from 'axios';
+import { screen } from '@testing-library/react';
 
-describe('Name for test suite', ()=> {
+import Button from '.';
+
+describe('Button', () => {
     beforeEach(() => {
-        renderWithReduxProvider(<App />) // , { initState } this might change, may also want to have it render a different init state based on the test
+        render(<Button />);
     })
 
-    test('What you want to test', () =>{
-        // expect().toBeInTheDocument();
-        // expect().toHaveBeenCalledTimes(1);
-        // expect().toBe();
-        // expect().toContain();
-        // expect().toEqual();
-        // expect().toBeInstanceOf();
+    test('renders a button', () => {
+        const btn = screen.getByRole('button')
+        expect(btn.textContent).toContain('');
     })
 })
