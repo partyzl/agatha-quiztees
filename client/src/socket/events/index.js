644e675235883a61a1fdc6143c923b1b0115a5f4
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-23 12:54:21 +0100

    penciled next question event

--- client/src/socket/events/index.js
+++ client/src/socket/events/index.js
@@ -1,14 +1,16 @@
 
-const { loadQuestions } = require("../../actions")
+const { loadQuestions, nextQuestion } = require("../../actions")
 const { store } = require("../../store")
 
 
 
 const QuizStarted = (questionInfo) => {
-    // const dispatch = useDispatch()
     store.dispatch(loadQuestions(questionInfo))
-    // dispatch(loadQuestions(questionInfo))
     document.location = '/quiz'
+}
+
+const nextQuestion = () => {
+    store.dispatch(nextQuestion)
 }
 
 module.exports = { QuizStarted }
