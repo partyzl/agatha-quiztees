8df35b72e9a1eefee848e28f69a91197706a8957
harrismawardi <harris.mawardi@yahoo.co.uk>
2021-09-23 15:36:07 +0100

    quiz working

--- client/src/socket/events/index.js
+++ client/src/socket/events/index.js
@@ -1,18 +1,6 @@
 const { loadQuestions, nextQuestion } = require("../../actions");
 const { store } = require("../../store");
 
-<<<<<<< HEAD
-const quizStarted = (questionInfo) => {
-    store.dispatch(loadQuestions(questionInfo))
-    document.location = '/quiz'
-}
-
-const incrementIndex = () => {
-    store.dispatch(nextQuestion)
-}
-
-module.exports = { quizStarted, incrementIndex }
-=======
 const QuizStarted = (questionInfo) => {
   store.dispatch(loadQuestions(questionInfo));
   //console.log(questionInfo);
@@ -28,4 +16,3 @@ const runTimer = (timer) => {
 };
 
 module.exports = { QuizStarted, incrementIdx, runTimer };
->>>>>>> 86bf1e7bb7cbad933ff9bb0ca229055b50156750
