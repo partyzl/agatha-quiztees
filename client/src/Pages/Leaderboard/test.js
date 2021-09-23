d7534a3d43ee1e391ed82c5d506296a0a5c4e18e
Simon Allan <stallan464@googlemail.com>
2021-09-23 10:39:20 +0100

    action testing

--- client/src/Pages/Leaderboard/test.js
+++ client/src/Pages/Leaderboard/test.js
@@ -1,20 +1,11 @@
-import { render } from 'react-dom'
-import {screen, waitFor} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import axios from 'axios'
-import App from '../../App'
+import Leaderboard from ".";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe('Name for test suite', ()=> {
-    beforeEach(() => {
-        renderWithReduxProvider(<App />) // , { initState } this might change, may also want to have it render a different init state based on the test
-    })
-
-    test('What you want to test', () =>{
-        // expect().toBeInTheDocument();
-        // expect().toHaveBeenCalledTimes(1);
-        // expect().toBe();
-        // expect().toContain();
-        // expect().toEqual();
-        // expect().toBeInstanceOf();
-    })
-})
+describe("Renders leaderboard", () => {
+  test("it renders a div", () => {
+    renderWithReduxProvider(<Leaderboard />);
+    const div = screen.getByRole("leaderboard");
+    expect(div).toBeInTheDocument();
+  });
+});
